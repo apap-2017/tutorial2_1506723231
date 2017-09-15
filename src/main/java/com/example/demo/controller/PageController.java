fa0cf85fb0be76682ad0fae1e19724485e37b5b6
teukuamru <teukuamrullahfaisal@gmail.com>
2017-09-15 14:20:03 +0700

    push for the first time

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+	
+	@RequestMapping("/perkalian")
+	public String greeting(@RequestParam(value = "a", required = false, defaultValue = "0") int a, @RequestParam(value = "b", required = false, defaultValue = "0") int b, Model model) {
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		return "perkalian1";
+	}
+}
